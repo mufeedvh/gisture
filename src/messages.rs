c87c8b69aee8bd4032076b48bbf0cb690d9e55ac
Mufeed VH <mufeedvh@gmail.com>
2021-12-18 15:33:13 +0530

    better concise this

--- src/messages.rs
+++ src/messages.rs
@@ -16,26 +16,12 @@ pub enum Type {
 
 /// Outputs logging messages
 pub fn push_message(log_type: Type, message: &str) {
-    let out: String;
-    
-    match log_type {
-        Type::Warning => {
-            let prefix = format!("{}{}{}", "[".bold(), "WARN".bold().yellow(), "]".bold());
-            out = format!("{} {}", prefix, message);
-        }
-        Type::Error => {
-            let prefix = format!("{}{}{}", "[".bold(), "ERROR".bold().red(), "]".bold());
-            out = format!("{} {}", prefix, message);
-        }
-        Type::Info => {
-            let prefix = format!("{}{}{}", "[".bold(), "INFO".bold().cyan(), "]".bold());
-            out = format!("{} {}", prefix, message);
-        }
-        Type::Success => {
-            let prefix = format!("{}{}{}", "[".bold(), "SUCCESS".bold().green(), "]".bold());
-            out = format!("{} {}", prefix, message);
-        }
-    }
+    let prefix = match log_type {
+        Type::Warning => format!("{}{}{}", "[".bold(), "WARN".bold().yellow(), "]".bold()),
+        Type::Error => format!("{}{}{}", "[".bold(), "ERROR".bold().red(), "]".bold()),
+        Type::Info => format!("{}{}{}", "[".bold(), "INFO".bold().cyan(), "]".bold()),
+        Type::Success => format!("{}{}{}", "[".bold(), "SUCCESS".bold().green(), "]".bold())
+    };
 
-    eprintln!("{}", out)
+    eprintln!("{}", format!("{} {}", prefix, message))
 }
